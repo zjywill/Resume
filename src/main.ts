0e96d8447b391f07c01c5a41e4e625683fd1d6b5
zjywill <zjywill@hotmail.com>
2025-02-01 13:14:21 +0800

    f

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
-import { createHead } from '@unhead/vue'; // <--
-
-const app = createApp(App);
-const head = createHead(); // <--
-app.use(head); // <--
-app.mount('#app');
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: 'class',
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography')],
-};
